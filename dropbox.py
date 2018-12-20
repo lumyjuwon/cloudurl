60058ac202384c6c871b7a3b61a483f88d20801b
An Do Eon <andoune0719@kookmin.ac.kr>
2018-12-20 14:43:13 +0900

    Create dropbox.py
    incomplete code

---
+++ dropbox.py
@@ -0,0 +1,25 @@
+from cloud import Cloud
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+class DropBox(Cloud):
+
+    def confirm_url(self, url):
+        url_split = url.split("/")
+        if url_split[2] == 'www.dropbox.com' and url_split[3] == 's':
+            self.url = url
+            self.request = requests.get(self.url)
+
+    def file_size(self, size_type=None):
+        # return self.conversion_size(file, size_type)
+        pass
+
+if "__main__" == __name__:
+    db = DropBox("https://www.dropbox.com/s/1rrmsgk8sv6n9xs/Dropbox%20%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0.pdf?dl=0")
+    # print(db.file_size('T'))
+    # print(db.file_size('G'))
+    # print(db.file_size('M'))
+    # print(db.file_size('K'))
+    # print(db.file_size('B'))
