507f87194bdbc57cc452c592e0f617ba8e0acbb3
An Do Eon <andoune0719@kookmin.ac.kr>
2018-12-20 14:41:07 +0900

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,8 @@
 class SizeTypeError(Exception):
     def __init__(self, size_type):
         raise Exception("\'" + size_type + "\'" + " don't support size-type you should input follow as \'B\', \'K\', \'M\', \'G\', \'T\' or Empty")
+
+
+class InvalidURLError(Exception):
+    def __init__(self, url):
+        raise Exception("\'" + url + "\'" + " is not valid URL")
